c307b64e4ba0e01c6bfb9cbeedb1e610c1b2b42f
Tomasz Mrugalski <thomson@klub.com.pl>
2007-01-09 23:16:54 +0000

    WMaxCtrl - first attempts.
    git-svn-id: https://klub.com.pl/var/svn/omnet@23 056025b2-ba0e-0410-831a-f040b6dfa49e

--- wimax/wmaxctrl.cc
+++ wimax/wmaxctrl.cc
@@ -8,11 +8,22 @@
  * 
  */
 
+#define FSM_DEBUG
 #include <omnetpp.h>
 #include <vector>
 #include "wmaxctrl.h"
 #include "wmaxmsg_m.h"
 
+#define TIMER(name, time, descr)  \
+    Timer##name##Value = time;			\
+    Timer##name        = new cMessage(descr);
+
+#define TIMER_START(name) \
+    scheduleAt(simTime() + Timer##name##Value, Timer##name);
+
+#define TIMER_STOP(name) \
+    if (Timer##name ->isScheduled()) \
+        cancelEvent(Timer##name);
 
 /********************************************************************************/
 /*** WMax Ctrl SS ****************************************************************/
@@ -41,12 +52,33 @@ WMaxCtrlSS::WMaxCtrlSS()
 
 void WMaxCtrlSS::initialize()
 {
+    TIMER(NetworkEntry, 0.0, "Start Network entry");
+    TIMER(Handover, 0.1, "Start handover");
 
+    TIMER_START(NetworkEntry);
+    TIMER_STOP(Handover);
 }
 
 void WMaxCtrlSS::handleMessage(cMessage *msg) 
 {
 
+    FSM_Switch(fsm)
+    {
+    case FSM_Exit(INIT):
+	/// @todo - network entry should be performed, not just jump to operational state
+	FSM_Goto(fsm, OPERATIONAL);
+    break;
+    case FSM_Enter(OPERATIONAL):
+    {
+        ev << "Network entry and service flows creation completed. SS is operational." << endl;
+        //scheduleAt(simTime()+TimerHandoverValue, TimerHandover);
+	break;
+    }
+    default: 
+    {
+
+    }
+  }
 }
 
 /********************************************************************************/
--- wimax/wmaxctrl.h
+++ wimax/wmaxctrl.h
@@ -8,6 +8,7 @@
  * 
  */
 
+#define FSM_DEBUG
 #include <omnetpp.h>
 #include <vector>
 #include "wmaxmsg_m.h"
@@ -47,6 +48,58 @@ public:
 protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
+
+    /******************************************/
+    /*** Finite State Machine *****************/
+    /******************************************/
+    cFSM fsm;
+    typedef enum {
+	INIT = 0,
+
+	// notice: transitive and steady states are numbered separately
+
+	// network entry
+	SEND_CDMA         = FSM_Transient(1),    // send CDMA code
+	WAIT_ANON_RNG_RSP = FSM_Steady(1),       // wait for anonymous RNG-RSP
+	SEND_RNG_REQ      = FSM_Transient(2),    // send RNG-REQ
+	WAIT_RNG_RSP      = FSM_Steady(2),       // wait for RNG-RSP
+	SEND_SBC_REQ      = FSM_Transient(3),    // send SBC-REQ
+	WAIT_SBC_RSP      = FSM_Steady(3),       // wait for SBC-RSP
+
+	/// @todo: PKM (TEKs)
+	SEND_REG_REQ      = FSM_Transient(4),    // send REG-REQ
+	WAIT_REG_RSP      = FSM_Steady(4),       // wait for REG-RSP
+
+	// service flow creation
+	INITIATE_SVC_FLOW_CREATION = FSM_Transient(5), // initialize service flow creation (i.e. start new FSMs for each flow)
+	WAIT_FOR_SVC_FLOW_COMPLETE = FSM_Steady(5),    // wait for service flow creation completion
+
+	OPERATIONAL       = FSM_Steady(6),             // network entry completed, service flows created, normal operation
+
+	// scanning
+	/// @todo - implement scanning
+
+	// handover
+	SEND_MSHO_REQ     = FSM_Transient(6),    // send MSHO-REQ
+	WAIT_BSHO_RSP     = FSM_Steady(7),       // wait for BSHO-RSP
+	SEND_HO_IND       = FSM_Transient(7),    // send HO-IND
+	HANDOVER_COMPLETE = FSM_Steady(8)        // handover complete
+    } State;
+
+    State OperationalOnEnter();
+    State OperationalOnExit();
+    State SendMshoReqOnEnter();
+    State SendMshoReqOnExit();
+
+    /******************************************/
+    /*** Finite State Machine *****************/
+    /******************************************/
+
+    double TimerHandoverValue; // when should the handover procedure start?
+    double TimerNetworkEntryValue;
+
+    cMessage * TimerNetworkEntry;
+    cMessage * TimerHandover;
 };
 
 class WMaxCtrlBS : public WMaxCtrl
