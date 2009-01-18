2cc901a80ad11a72f7e5d85443596b2918c04cce
Tomasz Mrugalski <thomson@klub.com.pl>
2009-01-18 00:19:39 +0000

    Link local <=> MAC address conversions, operator == added.
    git-svn-id: https://klub.com.pl/var/svn/omnet@148 056025b2-ba0e-0410-831a-f040b6dfa49e

--- util/ipv6.cc
+++ util/ipv6.cc
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include "logger.h"
 
 using namespace std;
 
@@ -35,11 +36,52 @@ string IPv6Addr::plain()
     return string(buf);
 }
 
-ostream & operator <<(ostream & s, IPv6Addr a) {
+void IPv6Addr::LinkLocalFromMAC(const uint64_t macAddr)
+{
+    memset(addr, 0, 16);
+    addr[15] = (unsigned char)((macAddr )      & 0xff);
+    addr[14] = (unsigned char)((macAddr >>  8) & 0xff);
+    addr[13] = (unsigned char)((macAddr >> 16) & 0xff);
+    addr[12] = 0xfe;
+    addr[11] = 0xff;
+    addr[10] = (unsigned char)((macAddr >> 24) & 0xff);
+    addr[9] = (unsigned char)((macAddr >> 32) & 0xff);
+    addr[8] = (unsigned char)((macAddr >> 40) & 0xff);
+    addr[1] = 0x80;
+    addr[0] = 0xfe;
+}
 
+uint64_t IPv6Addr::MacAddrFromLinkLocal()
+{
+    if ( (addr[0]!=0xfe) || (addr[1]!=0x80) )
+    {
+	return 0;
+    }
+    unsigned char tbl[6];
+    memcpy(tbl+3, addr+13,3);
+    memcpy(tbl, addr+8,3);
+
+    uint64_t m = ((uint64_t)tbl[5]) 
+	+ ((uint64_t)tbl[4] << 8)
+	+ ((uint64_t)tbl[3] << 16)
+	+ ((uint64_t)tbl[2] << 24)
+	+ ((uint64_t)tbl[1] << 32)
+	+ ((uint64_t)tbl[0] << 40);
+    return m;
+}
+
+ostream & operator <<(ostream & s, IPv6Addr a) 
+{
     char buf[80];
     inet_ntop(AF_INET6, a.addr, buf, 80);
     s << string(buf);
 
     return s;
+}
+
+bool IPv6Addr::operator==(const IPv6Addr &b)
+{
+    if (!memcmp(addr, b.addr, 16))
+	return true;
+    return false;
 }
--- util/ipv6.h
+++ util/ipv6.h
@@ -2,20 +2,25 @@
 #define IPV6MSG_H
 
 #include <ostream>
-#include <netinet/in.h>
+//#include <netinet/in.h>
 
 using namespace std;
 
 class IPv6Addr {
 public:
     IPv6Addr();
-    IPv6Addr(const char a[16], bool plain);
+    IPv6Addr(const char *a, bool plain);
     IPv6Addr(unsigned int a, unsigned int b, unsigned int c, unsigned int d);
-    char addr[16];
+    uint64_t MacAddrFromLinkLocal();
+    void LinkLocalFromMAC(uint64_t mac);
+    unsigned char addr[16];
     
     string plain();
+
+    bool operator ==(const IPv6Addr &b);
 };
 
 ostream & operator <<(ostream & s, IPv6Addr);
+
 
 #endif
