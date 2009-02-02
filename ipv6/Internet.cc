c870ceea330dfd9dee35f325207f865eb2d435c3
Tomasz Mrugalski <thomson@klub.com.pl>
2009-02-02 01:17:15 +0000

    Internet module should now record number of transmitted packets properly.
    git-svn-id: https://klub.com.pl/var/svn/omnet@198 056025b2-ba0e-0410-831a-f040b6dfa49e

--- ipv6/Internet.cc
+++ ipv6/Internet.cc
@@ -94,7 +94,7 @@ void Internet::handleMessage(cMessage *msg)
 	    Log(Debug) << "Route for dst addr=" << dst << " found: " << *it << LogEnd;
 	    sendDelayed(msg, delay, "ipOut", it->gateIndex);
 	    MsgsTransmittedCnt++;
-	    if ( (MsgsTransmittedCnt>1000) && (!MsgsTransmittedCnt%100) )
+	    if ( ( MsgsTransmittedCnt<=1000) || ( (MsgsTransmittedCnt>1000) && (!MsgsTransmittedCnt%100) ) )
 		MsgsTransmittedVector.record(MsgsTransmittedCnt);
 	    return;
 	}
